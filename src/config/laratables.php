ffa9245515ce753fb58284534b9f35bebcd23588
Gaurav Makhecha <gauravmakhecha@gmail.com>
2018-08-08 10:07:38 +0530

    Add config option for row id prefix

--- src/config/laratables.php
+++ src/config/laratables.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+    // Prefix for the DT_RowId value for each datatables record.
+    'row_id_prefix' => 'laratables_row_',
+
+    // Display format of dates (carbon instances) of the table.
     'date_format' => 'Y-m-d H:i:s',
 ];
