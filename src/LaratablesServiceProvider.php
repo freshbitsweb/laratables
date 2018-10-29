24bd8d9e5872cebd7a86f2a0138f59bc68019aa6
Gaurav Makhecha <gauravmakhecha@gmail.com>
2018-10-29 19:02:15 +0530

    Publish only if request is from console

--- src/LaratablesServiceProvider.php
+++ src/LaratablesServiceProvider.php
@@ -13,9 +13,11 @@ class LaratablesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/config/laratables.php' => config_path('laratables.php'),
-        ], 'laratables_config');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/laratables.php' => config_path('laratables.php'),
+            ], 'laratables_config');
+        }
     }
 
     /**
